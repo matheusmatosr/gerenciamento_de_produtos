5c1f77cec5613d76658953ee4eb0802a24461859
Matheus Matos <100291312+matheusmatosr@users.noreply.github.com>
2025-01-02 11:49:06 -0300

    first

---
+++ src/app/layout.js
@@ -0,0 +1,32 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import 'primereact/resources/themes/lara-light-indigo/theme.css';
+import 'primereact/resources/primereact.min.css';                
+import 'primeicons/primeicons.css';  
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Gerenciamento de Produtos",
+  description: "Desenvolvido por Matheus Matos",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
